d282c485b4067042469d9b39e86dcc3a7fa74c82
Homyakov, Ilya2 <i2.homyakov@itransition.com>
2018-12-17 09:35:06 +0300

    Initial Commit

---
+++ ITRUIDynamicsTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ITRUIDynamicsTest
+//
+//  Created by Homyakov, Ilya2 on 17/12/2018.
+//  Copyright © 2018 Homyakov, Ilya2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
