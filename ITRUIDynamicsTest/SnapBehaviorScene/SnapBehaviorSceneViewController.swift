692dd9d363d892a06e0463ad77efa3733dff386f
Homyakov, Ilya2 <i2.homyakov@itransition.com>
2018-12-17 11:26:06 +0300

    Done

--- ITRUIDynamicsTest/SnapBehaviorScene/SnapBehaviorSceneViewController.swift
+++ ITRUIDynamicsTest/SnapBehaviorScene/SnapBehaviorSceneViewController.swift
@@ -9,9 +9,57 @@
 import UIKit
 
 class SnapBehaviorSceneViewController: UIViewController {
+    @IBOutlet private weak var cardView: UIView!
+
+    private var animator: UIDynamicAnimator?
+    private var snapping: UISnapBehavior?
+
+    public init() {
+        super.init(nibName: "\(SnapBehaviorSceneViewController.self)", bundle: nil)
+    }
+
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupAnimator()
+        setupCardView()
     }
 
+    private func setupAnimator() {
+        let animator = UIDynamicAnimator(referenceView: view)
+        self.animator = animator
+        let snapping = UISnapBehavior(item: cardView, snapTo: view.center)
+        self.snapping = snapping
+        animator.addBehavior(snapping)
+    }
+
+    private func setupCardView() {
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(pannedCardView))
+        cardView.addGestureRecognizer(panGesture)
+        cardView.isUserInteractionEnabled = true
+    }
+
+    @objc
+    private func pannedCardView(recognizer: UIPanGestureRecognizer) {
+        guard let snapping = snapping else {
+            return
+        }
+        switch recognizer.state {
+        case .began:
+            animator?.removeBehavior(snapping)
+        case .changed:
+            let translation = recognizer.translation(in: view)
+            cardView.center = CGPoint(x: cardView.center.x + translation.x,
+                                      y: cardView.center.y + translation.y)
+            recognizer.setTranslation(.zero, in: view)
+        case .ended, .cancelled, .failed:
+            animator?.addBehavior(snapping)
+        case .possible:
+            break
+        }
+    }
 }
--- ITRUIDynamicsTest/ViewController.swift
+++ ITRUIDynamicsTest/ViewController.swift
@@ -12,9 +12,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func snapBehaviorSceneButtonTuchUpInside(_ sender: Any) {
+        self.show(SnapBehaviorSceneViewController(), sender: sender)
+    }
+}
 
+extension UIView { // FIXME: move
+    class public func fromNib<T: UIView>() -> T {
+        if let xib = Bundle.main.loadNibNamed(String(describing: T.self), owner: nil, options: nil),
+            let result = xib.first as? T {
+            return result
+        }
+        return T(frame: .zero)
+    }
 }
 
